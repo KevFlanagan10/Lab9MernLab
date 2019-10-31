ce830f5d5e455e3be1915f99e2bb62a581d73a7b
KevFlanagan10 <G00228079@gmit.ie>
2019-10-31 16:36:50 +0000

    first commit

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -5,6 +5,10 @@ const path = require('path');
 
 const cors = require('cors');//npm install cors
 
+const mongoose = require('mongoose');//npm install mongoose
+const mongoDB = 'mongodb+srv://admin:admin@cluster0-dapzk.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(mongoDB,{useNewUrlParser:true})
+
 app.use(cors());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -17,6 +21,18 @@ app.use(function(req, res, next) {
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended:false}));//to use body parser
 app.use(bodyParser.json());
+
+//Schema for the database
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema({
+        title:String,
+        year:String,
+        poster:String 
+})
+
+const MovieModel = mongoose.model('movie', movieSchema);
+
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
@@ -47,29 +63,52 @@ app.post('/name', (req, res)=>{
     res.send('Hello from Post ' + req.body.firstname + ' ' + req.body.lastname);
 })
 
-app.get('/api/movies',(req, res)=>{ //listening to the get method!!
-    const myMovies = [
+//listening to the get method!!
+app.get('/api/movies',(req, res)=>{
+     MovieModel.find((error, data)=>
+     {
+         res.json(data);
+        })
 
-            {
-            "Title":"Avengers: Infinity War",
-            "Year":"2018",
-            "Poster":"https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
-            },
-            {
-            "Title":"Captain America: Civil War",
-            "Year":"2016",
-            "Poster":"https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
-            }
-            ];
-    res.status(200).json({movies:myMovies,
-         message:'hello from the other side'});
-})
+    // const myMovies = [
+
+    //         {
+    //         "Title":"Avengers: Infinity War",
+    //         "Year":"2018",
+    //         "Poster":"https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg"
+    //         },
+    //         {
+    //         "Title":"Captain America: Civil War",
+    //         "Year":"2016",
+    //         "Poster":"https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
+    //         }
+    //         ];
+    // res.status(200).json({movies:myMovies,
+    //      message:'hello from the other side'});
+
+    })
+
+    app.get('/api/movies:id', (req, res)=>{
+        console.log(req.params.id);
+
+        MovieModel.findById(req.params.id,(error,data)=>{
+            res.json(data);
+        })
+    })
 
 app.post('/api/movies', (req,res)=>{
     console.log('Post Request Host sucessful');
     console.log(req.body.title);
     console.log(req.body.year);
     console.log(req.body.poster);
+
+    MovieModel.create({
+        title:req.body.title,
+        year:req.body.year,
+        poster:req.body.poster
+    });
+
+
     res.json('post recieved');
 })
 
